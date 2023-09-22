5e4f5143882fcaf2e225828d27616083a53b765f
Maks12423415 <126457937+Maks12423415@users.noreply.github.com>
2023-09-22 16:29:43 +0200

    .

--- klient/app.js
+++ klient/app.js
@@ -1,67 +1,33 @@
+let continentsData = [];
+
 async function fetchContinents() {
-    try {
-      const response = await fetch("http://192.168.15.13:3000/kontynenty");
-      const continents = await response.json();
-  
-      // Pobierz kontener, do którego będziesz dodawać checkboxy
-      const container = document.getElementById("checkboxContainer");
-  
-      // Iteruj przez kontynenty i twórz checkboxy
-      continents.forEach((continent) => {
-        const checkBox = document.createElement("input");
-        checkBox.type = "checkbox";
-        checkBox.id = continent.kontynent; // Ustaw id checkboxa na nazwę kontynentu
-        checkBox.value = continent.kontynent; // Ustaw wartość checkboxa na nazwę kontynentu
-        checkBox.name = "continents"; // Ustaw nazwę grupy checkboxów, jeśli potrzebujesz grupować je razem
-  
-        // Dodaj etykietę do checkboxa
-        const label = document.createElement("label");
-        label.textContent = continent.kontynent;
-        label.htmlFor = continent.kontynent; // Połącz etykietę z checkboxem za pomocą "for" i "id"
-  
-        // Dodaj checkbox i etykietę do kontenera
-        container.appendChild(checkBox);
-        container.appendChild(label);
-      });
-    } catch (error) {
-      console.error("Błąd podczas pobierania danych o kontynentach", error);
-    }
-  }
-  
-  // Wywołaj funkcję, aby pobrać kontynenty i utworzyć checkboxy po załadowaniu strony
-  fetchContinents();
+  try {
+    const response = await fetch("http://localhost:3000/kontynenty");
+    const json = await response.json();
 
-  
+    continentsData = json;
 
-  // Funkcja do filtrowania i wyświetlania państw na podstawie kontynentu
-function filterCountriesByContinent(checkboxes) {
-    // Pobierz wszystkie zaznaczone kontynenty
-    const selectedContinents = Array.from(checkboxes)
-      .filter((checkbox) => checkbox.checked)
-      .map((checkbox) => checkbox.getAttribute("data-continents"));
-  
-    // Jeśli nie wybrano żadnych kontynentów, pokaż wszystkie państwa
-    if (selectedContinents.length === 0) {
-      // Tutaj możesz wyświetlić wszystkie państwa lub zresetować wcześniejszy wynik
-    } else {
-      // W przeciwnym razie filtrować państwa na podstawie wybranych kontynentów
-      const filteredCountries = countries.filter((country) =>
-        selectedContinents.includes(country.kontynent)
-      );
-  
-      // Tutaj możesz wyświetlić państwa na stronie
-      console.log(filteredCountries);
-    }
-  }
-  
-  // Pobierz wszystkie checkboxy kontynentów
-  const continentCheckboxes = document.querySelectorAll('[data-continents]');
-  
-  // Dodaj obsługę zdarzenia zmiany stanu checkboxa
-  continentCheckboxes.forEach((checkbox) => {
-    checkbox.addEventListener("change", () => {
-      // Po zmianie stanu checkboxa wywołaj funkcję do filtrowania państw
-      filterCountriesByContinent(continentCheckboxes);
+    const checkboxContainer = document.getElementById("checkbox-container");
+
+    json.forEach((continent, index) => {
+      const checkBox = document.createElement("input");
+      checkBox.type = "checkbox";
+      checkBox.id = `checkbox-${index}`;
+      checkBox.value = continent.nazwa;
+
+      const label = document.createElement("label");
+
+      // Dodaj tekst z nazwą kontynentu do etykiety
+      label.textContent = continent.nazwa;
+
+      // Dodaj checkbox i etykietę do kontenera
+      checkboxContainer.appendChild(checkBox);
+      checkboxContainer.appendChild(label);
     });
-  });
-  
+  } catch (error) {
+    console.error("Wystąpił błąd podczas pobierania danych o kontynentach:", error);
+  }
+}
+
+fetchContinents();
+
--- serwer/api.js
+++ serwer/api.js
@@ -1,31 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-const mysql = require("mysql")
+const mysql = require("mysql");
 const port = 3000;
 const app = express();
 
 app.use(cors());
 
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kraje"
+});
 
-var conn= mysql.createConnection({
+conn.connect((err) => {
+  if (err) console.log(err);
+  else console.log("połączono z bazą");
+});
 
-    host:"localhost",
-    user: "root",
-    password:"",
-    database:"kraje"
-    })
-  
-    conn.connect((err) => {
-      if (err) console.log(err);
-      else console.log("połączono z bazą");
-    });
-
-
-    app.get("/", function (req, res) {
-        res.send("ok");
-        console.log("zapytanie");
-      });
+app.get("/", function (req, res) {
+  res.send("ok");
+  console.log("zapytanie");
+});
 
 
       
@@ -75,7 +71,34 @@ var conn= mysql.createConnection({
         });
       });
 
+      app.get("/all", function (req, res) {
+        const sql = 'SELECT *  FROM panstwo';
+        conn.query(sql, (err, results, fields) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Błąd bazy danych');
+            return;
+          }
+          console.log(results);
+          // Wysłać wyniki jako odpowiedź na żądanie HTTP.
+          res.json(results);
+        });
+      });
 
+
+      app.get("/max", function (req, res) {
+        const sql = 'SELECT MAX(populacja) FROM panstwo';
+        conn.query(sql, (err, results, fields) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Błąd bazy danych');
+            return;
+          }
+          console.log(results);
+          // Wysłać wyniki jako odpowiedź na żądanie HTTP.
+          res.json(results);
+        });
+      });
 
       app.listen(port, () => {
         console.log(`Serwer działa na porcie ${port}`);
